ee97821dc43188be70e56ef3758448ed2ccd5fc9
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-07-11 12:34:55 +0200

    dropdown list

--- 3/main.js
+++ 3/main.js
@@ -2,13 +2,12 @@ const nav = document.querySelector('.nav');
 const menu = document.querySelector('.menu');
 
 nav.addEventListener('mouseover', () => {
-    menu.style.transition = `ease-in-out 2s 2s 2s`;
     menu.classList.add('active');
+    menu.addEventListener('mouseover', () => {
+        menu.classList.add('active');
+    })
 });
 
-nav.addEventListener('mouseout', () => {
-    // setTimeout(() => menu.classList.remove('active'), 100);
-    menu.style.transition = `ease-in-out 1s 1s`;
+menu.addEventListener('mouseout', () => {
     menu.classList.remove('active');
-
 })
