7d895c6940dd7f1cc42293a001fb5d2990d63165
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-07-15 01:08:12 +0200

    dark mode

--- 5/script.js
+++ 5/script.js
@@ -2,16 +2,21 @@ const mode = document.querySelector('.mode');
 const lightMode = document.querySelector('.light');
 const darkMode = document.querySelector('.dark');
 const modeText = document.querySelector('.mode-text');
+const body = document.querySelector('body');
 
-function changeMode(e) {
+function changeMode() {
     if (lightMode.classList.contains('active')) {
         lightMode.classList.remove('active');
         darkMode.classList.add('active');
         modeText.innerHTML = `Change mode from dark to light`;
+        body.classList.add('dark-body');
+        body.classList.remove('light-body');
     } else {
         darkMode.classList.remove('active');
         lightMode.classList.add('active');
         modeText.innerHTML = `Change mode from light to dark`;
+        body.classList.add('light-body');
+        body.classList.remove('dark-body');
     }
 }
 
