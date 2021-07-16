bafcca56066f6131a00f7b6598c75dbe9ad8fc37
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-07-17 00:49:42 +0200

    up button and almost down

--- 7/script.js
+++ 7/script.js
@@ -1,17 +1,41 @@
 const upBtn = document.querySelector('.up');
 const downBtn = document.querySelector('.down');
 const items = document.querySelectorAll('.item');
+let number = 0;
 
 upBtn.addEventListener('click', () => {
     items.forEach(item => {
-        if (item.style.display === 'block') {
-            item.style.display = 'none';
-        } else if (item.style.display === 'none') {
-            item.style.display = 'block';
+        if (item.classList.contains('active')) {
+            number = item.dataset.number;
+            item.classList.remove('active');
+            number++;
         }
-    })
+
+    });
+
+    if (number === items.length) {
+        number = 0;
+    }
+
+    items[number].classList.add('active');
+    number++;
+    items[number++].classList.add('active');
+
 });
 
 downBtn.addEventListener('click', () => {
+    items.forEach(item => {
+        if (item.classList.contains('active')) {
+            number = item.dataset.number;
+            item.classList.remove('active');
+            number--;
+            if (number === 0) number = items.length - 1;
+        }
+    });
+    items[number].classList.add('active');
+    if (number === 0) number = items.length - 1;
+    // if (number === items.length ) number = 0;
 
+    number--;
+    items[number--].classList.add('active');
 });
