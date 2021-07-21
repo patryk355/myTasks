60fb0384c77b790cde65cb2fb11a88ec16013c70
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-07-21 21:21:13 +0200

    actual number in class in li

--- toDoList/script.js
+++ toDoList/script.js
@@ -4,51 +4,65 @@ const ul = document.querySelector('.list');
 const addInput = document.querySelector('.add-input');
 const form = document.querySelector('.form');
 const info = document.querySelector('.info');
-let number = 0;
+let number = 1;
 
 
 //Add item to list
-function addItem() {
+function addItem(e) {
+    e.preventDefault();
     if (addInput.value == '') {
         info.textContent = 'Wpisz przynajmniej jeden znak';
     } else {
         info.textContent = '';
-        number++;
-
+        number = ul.children.length + 1;
+        
         // Create list item
         const listItem = document.createElement('li');
         ul.appendChild(listItem);
-        listItem.classList.add(`item`);
-        listItem.classList.add(`item-${number}`);
+        listItem.classList.add(`item`, `item-${number}`);
         listItem.textContent = `${addInput.value}`;
 
         // Create delete button in li
         const delBtn = document.createElement('button');
         listItem.appendChild(delBtn);
-        delBtn.classList.add(`del`);
-        delBtn.classList.add(`del-${number}`);
+        delBtn.classList.add(`del`, `del-${number}`);
+
         const iconBtn = document.createElement('i');
         delBtn.appendChild(iconBtn);
-        iconBtn.classList.add('fas');
-        iconBtn.classList.add('fa-trash-alt');
+        iconBtn.classList.add('fas', 'fa-trash-alt');
 
         addInput.value = '';
+
+        number++;
 
         delBtn.addEventListener('click', removeItem);
     }
 
 }
 
-// const removeItem = (e) => {
-//     e.target.parentNode.remove();
-// }
-
 
 // Delete list item with button
 function removeItem(e) {
+    // e.preventDefault();
+    let actualNumber = 1;
     this.parentNode.remove();
+
+    // Actual number in class
+    const items = document.querySelectorAll('.item');
+    console.log(items);
+    items.forEach(item => {
+        // Remove old class
+        item.classList.remove(item.classList.item(1));
+        console.log(item.classList.item(1));
+
+        // Add actual class
+        item.classList.add(`item-${actualNumber}`);
+        actualNumber++;
+        number++;
+    })
 }
 
+form.addEventListener('submit', addItem);
 btnAdd.addEventListener('click', addItem);
 
 
